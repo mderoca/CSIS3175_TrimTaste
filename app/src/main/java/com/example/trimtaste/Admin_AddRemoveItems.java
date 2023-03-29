f94209a3508b05ed43f1811ae03fa88da46b8e11
jordiloo <jordansidneydunu@gmail.com>
2023-03-29 14:28:13 -0700

    second commit

--- app/src/main/java/com/example/trimtaste/Admin_AddRemoveItems.java
+++ app/src/main/java/com/example/trimtaste/Admin_AddRemoveItems.java
@@ -1,62 +1,107 @@
 package com.example.trimtaste;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 public class Admin_AddRemoveItems extends AppCompatActivity {
-    String[] food = {"Seafood Pizza \n $10.00", "Burger \n $12.00", "Stir-fry spaghetti \n $15.00", "Chicken&Celery \n $10.00", "pesto pasta \n $20.00"};
-    int[] images = {R.drawable.seafood_pizza, R.drawable.mock_res1_item1, R.drawable.mock_res1_item2, R.drawable.mock_res1_item3, R.drawable.mock_res1_item4};
+
+    private List<FoodItem> foodItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_remove_food_items);
-        ArrayList<HashMap<String, String>> aList =
-                new ArrayList<HashMap<String, String>>();
+        Button btnAddItems = findViewById(R.id.btnAddItems);
+        // Initialize food items
+        initFoodItems();
 
-        for (int i = 0; i < food.length; i++) {
-            HashMap<String, String> hashMap = new HashMap<>();
-            hashMap.put("txt", food[i]);
-            hashMap.put("images", Integer.toString(images[i]));
-            aList.add(hashMap);
-        }
-        String[] from = {"images", "txt"};
-        int[] to = {R.id.image, R.id.txtFoodItem};
+        // Set up RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new FoodItemAdapter());
 
-        SimpleAdapter adapter = new SimpleAdapter(Admin_AddRemoveItems.this,
-                aList, R.layout.listview_layout, from, to);
-
-        ListView listView = findViewById(R.id._dynamic);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Set up add button
+        findViewById(R.id.btnAddItems).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view,
-                                    int position, long id) {
-                switch (position) {
-                    case 0:
-                        startActivity(new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("https://www.dominos.ca/?gclid=CjwKCAjw_MqgBhAGEiwAnYOAemmCfgrWvNvdvCRtfkOpbUS2KIm11Zb8uWdD8iJNgMj0kZBNJy8SdxoCP_MQAvD_BwE&gclsrc=aw.ds")));
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-
-                }
-
+            public void onClick(View v) {
+                // Handle add button click
             }
         });
+    }
+
+    private void initFoodItems() {
+        String[] food = {"Seafood Pizza \n $10.00", "Cajun Chicken Burger \n $12.00", "Stir-fry spaghetti \n $15.00", "Chicken&Celery \n $10.00", "pesto pasta \n $20.00"};
+        int[] images = {R.drawable.seafood_pizza, R.drawable.mock_res1_item1, R.drawable.mock_res1_item2, R.drawable.mock_res1_item3, R.drawable.mock_res1_item4};
+
+        for (int i = 0; i < food.length; i++) {
+            FoodItem foodItem = new FoodItem();
+            foodItem.setText(food[i]);
+            foodItem.setImage(images[i]);
+            foodItems.add(foodItem);
+        }
+    }
+
+    private class FoodItemAdapter extends RecyclerView.Adapter<FoodItemViewHolder> {
+
+        @Override
+        public FoodItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new FoodItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(FoodItemViewHolder holder, int position) {
+            FoodItem foodItem = foodItems.get(position);
+            holder.imageView.setImageResource(foodItem.getImage());
+            holder.textView.setText(foodItem.getText());
+        }
+
+        @Override
+        public int getItemCount() {
+            return foodItems.size();
+        }
+    }
+
+    private class FoodItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView imageView;
+        TextView textView;
+
+        public FoodItemViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+            textView = itemView.findViewById(R.id.txtFoodItem);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            switch (position) {
+                case 0:
+                case 1:
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.dominos.ca/?gclid=CjwKCAjw_MqgBhAGEiwAnYOAemmCfgrWvNvdvCRtfkOpbUS2KIm11Zb8uWdD8iJNgMj0kZBNJy8SdxoCP_MQAvD_BwE&gclsrc=aw.ds")));
+                    break;
+                case 2:
+                    // Handle item 2 click
+                    break;
+                // Handle additional items
+            }
+        }
     }
 }
